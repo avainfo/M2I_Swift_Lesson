cc5c1633931125050e85d44dce7a2fb82ef66dcf
AvaInfo <avainfoconseils@gmail.com>
2025-10-10 11:32:23 +0200

    Firebase

--- M2I_Swift_Lesson/ContentView.swift
+++ M2I_Swift_Lesson/ContentView.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
+    private let db: Firestore
     private var user: User = User()
     @State var isShown: Bool = false
     @StateObject private var counterModel: CounterModel = CounterModel()
     
+    init() {
+        db = Firestore.firestore()
+    }
     
     var body: some View {
         
@@ -25,36 +30,34 @@ struct ContentView: View {
                 Spacer()
                 Footer(counterModel: counterModel)
                 Button(action: {
-                    print(counterModel.counter)
-                    isShown = true
+                    Task {
+                        await addDataDb()
+                    }
                 }) {
                     Text("Invite & earn")
                         .padding()
                         .background(.yellow)
                         .cornerRadius(50)
                 }
-                .alert("Test", isPresented: $isShown) {
-                    Button("OK", role: .cancel) { }
-                    Button("Refuser", role: .destructive) { }
-
-//                    if #available(iOS 26.0, *) {
-//                        Button(role: .cancel, action: {isShown = false})
-//                    } else {
-//                        // Fallback on earlier versions
-//                    }
-//                    if #available(iOS 26.0, *) {
-//                        Button(role: .destructive, action: {isShown = false})
-//                    } else {
-//                        // Fallback on earlier versions
-//                    }
-                }
                 Spacer()
                 Spacer()
             }
         }
     }
-}
-
-#Preview {
-    ContentView()
+    
+    func addDataDb() async {
+        let ref = db.collection("users").addDocument(data: [
+          "first": "Ada2",
+          "last": "Lovelace2",
+          "born": 18152
+        ])
+        let doc = db.collection("users").document(ref.documentID)
+        print("Document added with ID: \(ref.documentID)")
+        do {
+            let x = try await doc.getDocument().data()
+            print(x)
+        }catch {
+            print("Erreur : \(error)")
+        }
+    }
 }
